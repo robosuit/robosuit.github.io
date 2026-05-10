18046803c001c01129f98b341872494a6f49e4d6
robosuit <evilanubis0@gmail.com>
2026-05-10 14:45:16 -0400

    Updated Localhost build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const siteBase = process.env.SITE_BASE ?? (process.env.npm_lifecycle_event === 'dev' ? '/' : '/robosuit-astro/');
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: siteBase,
+});
